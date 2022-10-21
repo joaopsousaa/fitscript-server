a2e7a63d8a417b42f95325232d6d8a427e787e7e
Abhijeet Dalvi <abhijeetvdalvi@.com>
2022-10-22 00:12:41 +0200

    duplicate files deleted

--- routes/workout.routes 2.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const { isAuthenticated } = require("../middleware/jwt.middleware");
-const { Workout } = require("../models/Workout.model");
-
-const workoutRoutes = Router();
-
-workoutRoutes.get("/", isAuthenticated, (req, res) => {});
