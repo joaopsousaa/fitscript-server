bbe4420e5a132b36fad5aaf75dcc601ad1cfc987
joaopsousaa <104941882+joaopsousaa@users.noreply.github.com>
2022-10-25 18:37:07 +0100

    api call fix

--- utils/index.js
+++ utils/index.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
 
-const { RAPID_EXERCISE_API_URL, RAPID_EXERCISE_API_KEY } = require("./consts");
+const { RAPID_EXERCISE_API_URL } = require("./consts");
 
 const options = {
   method: "GET",
   url: RAPID_EXERCISE_API_URL,
   headers: {
-    "X-RapidAPI-Key": RAPID_EXERCISE_API_KEY,
+    "X-RapidAPI-Key": process.env.RAPID_EXERCISE_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
