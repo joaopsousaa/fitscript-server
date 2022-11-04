4abe36764981962f9466da0ef214256b21a78d92
joaopsousaa <104941882+joaopsousaa@users.noreply.github.com>
2022-11-04 20:39:18 +0000

    fix cors error

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,27 @@
 const router = require("express").Router();
-
+const { isAuthenticated } = require("../middleware/jwt.middleware");
+const Workout = require("../models/Workout.model");
+const User = require("../models/User.model");
+const { BAD_REQUEST } = require("../utils/status.codes");
 /* GET home page */
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+// router.get("/", (req, res, next) => {
+//   res.json("All good in here");
+
+// });
+
+router.get("/", isAuthenticated, async (req, res) => {
+  const userId = req.payload._id;
+  const arrayOfWorkoutsDataFromUser = [];
+
+  const user = await User.findById(userId);
+  const getUserWorkouts = await Promise.all(
+    user.workouts.map(async (workoutId) => {
+      const workout = await Workout.findById(workoutId);
+      arrayOfWorkoutsDataFromUser.push(workout);
+    })
+  );
+  return res.json(arrayOfWorkoutsDataFromUser);
 });
 
 module.exports = router;
