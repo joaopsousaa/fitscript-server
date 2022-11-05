0365aaf0c38b4841e2809f2e82315ccb74604b51
Abhijeet Dalvi <abhijeetvdalvi@.com>
2022-11-05 10:14:45 +0100

    dashboard

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,21 +1,21 @@
-// const router = require("express").Router();
-// const { isAuthenticated } = require("../middleware/jwt.middleware");
-// const Workout = require("../models/Workout.model");
-// const User = require("../models/User.model");
-// const { BAD_REQUEST } = require("../utils/status.codes");
+const router = require("express").Router();
+const { isAuthenticated } = require("../middleware/jwt.middleware");
+const Workout = require("../models/Workout.model");
+const User = require("../models/User.model");
+const { BAD_REQUEST } = require("../utils/status.codes");
 
-// router.get("/", isAuthenticated, async (req, res) => {
-//   const userId = req.payload._id;
-//   const arrayOfWorkoutsDataFromUser = [];
+router.get("/", isAuthenticated, async (req, res) => {
+  const userId = req.payload._id;
+  const arrayOfWorkoutsDataFromUser = [];
 
-//   const user = await User.findById(userId);
-//   const getUserWorkouts = await Promise.all(
-//     user.workouts.map(async (workoutId) => {
-//       const workout = await Workout.findById(workoutId);
-//       arrayOfWorkoutsDataFromUser.push(workout);
-//     })
-//   );
-//   return res.json(arrayOfWorkoutsDataFromUser);
-// });
+  const user = await User.findById(userId);
+  const getUserWorkouts = await Promise.all(
+    user.workouts.map(async (workoutId) => {
+      const workout = await Workout.findById(workoutId);
+      arrayOfWorkoutsDataFromUser.push(workout);
+    })
+  );
+  return res.json(arrayOfWorkoutsDataFromUser);
+});
 
-// module.exports = router;
+module.exports = router;
