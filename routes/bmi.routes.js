526b9fb50a3f5d281cb78f956f4dcd011eb51643
Abhijeet Dalvi <abhijeetvdalvi@.com>
2022-11-10 21:27:06 +0100

    some changes

--- routes/bmi.routes.js
+++ routes/bmi.routes.js
@@ -6,18 +6,10 @@ const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 router.post("/", isAuthenticated, async (req, res) => {
   const requestBody = req.body;
-  //   console.log(requestBody);
+  console.log(requestBody);
   const bmiResult = await getBmiDataFromApi(requestBody);
-  //   const { data } = bmiResult;
-  //   console.log("THIS IS data:", data);
-  //   Bmi.create({ ...data })
-  //     .then((bmi) => {
-  //       console.log("THIS IS BMI", bmi);
+
   return res.json(bmiResult);
-  //     })
-  //     .catch(() => {
-  //       return res.status(BAD_REQUEST);
-  //     });
 });
 
 module.exports = router;
